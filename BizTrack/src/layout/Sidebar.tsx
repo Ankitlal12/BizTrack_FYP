044bf6b4de74ddf960733c223acbe630f18443c9
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-11-05 20:13:01 +0545

    title: dragging the folder
    dragging the layout folder and page folder inside src folder
    
    adding react-icons logo in sidebar page

---
+++ BizTrack/src/layout/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React, { useCallback, useMemo, useState, memo } from 'react'
+import { NavLink } from 'react-router-dom'
+ 
+import { FaHistory } from "react-icons/fa";
+import { TbLayoutDashboard } from "react-icons/tb";
+import { FiPackage } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { FiFileText } from "react-icons/fi";
+import { CiSettings } from "react-icons/ci";
+import { LuShoppingBag } from "react-icons/lu";
+import { IoBarChartSharp } from "react-icons/io5";
+import { FiMenu } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
+
+
+
