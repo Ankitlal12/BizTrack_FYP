f95501c9e2c1024691066598a128c2c8ac1cff8e
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-11-08 21:51:37 +0545

    adding handle submit button adn error

--- BizTrack/src/Pages/Login.tsx
+++ BizTrack/src/Pages/Login.tsx
@@ -9,6 +9,29 @@ const Login = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
   const [password,setPassword]=useState("");
+  const [error,setError]=useState('');
+  const[isLoading,setIsLoading]=useState(false);
+
+  const handleSubmit=async(e:React.FormEvent)=>{
+    e.preventDefault()
+    setError('')
+    setIsLoading(true)
+    try{
+        const succcess =await login(username,password)
+        if(succcess){
+            navigate('/',{
+                replace:true,
+            })
+        }else{
+            setError('Invalid username or password')
+        }
+        
+    }catch(err){
+            setError('An error occured. Please try again. ')
+        }finally{
+            setIsLoading(false)
+        }
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -20,7 +43,15 @@ const Login = () => {
             Inventory & Business Management System
           </p>
         </div>
-
+        <div className="bg-white p-8 rounded-lg shadow-md">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+                {error && (
+                   <div className="bg-red-50 border-l-4     border-red-500 p-4 rounded-md">
+                     <p className="text-sm text-red-700">{error}</p>
+                   </div>
+                )}
+            </form>
+        </div>
         <div>
           <label
             htmlFor="username"
