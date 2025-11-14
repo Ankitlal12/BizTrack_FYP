b76e510b2bf7daa568837c1ccea5956043698ba1
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-11-14 12:15:38 +0545

    adding details in dashboard page for login

---
+++ Frontend/src/Pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+
+const Dashboard=()=>{
+    return (
+        <Layout>
+            <div className='p-4'>
+                <h2 className="text-2xl font-semibold text-gray-800">Dashboard</h2>
+                <p className='text-gray-600 mt-2'>Welcome to your Dashboard</p>
+            </div>
+        </Layout>    
+    );
+};
+
+export default Dashboard;
